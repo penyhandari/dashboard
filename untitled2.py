bd456000ea71c761d66131a0175b0fa33f44b8b7
penyhandari <penydwihandari@gmail.com>
2026-06-11 17:36:10 +0700

    Add files via upload

--- untitled2.py
+++ untitled2.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1UYUSnbRuJANHybESnjrMJ_GCASYH68t_
 """
 
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
